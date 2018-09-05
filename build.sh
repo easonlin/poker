3a091606517c12fc992341ba028115f631d9c79f
Eason Lin (RD-TW) <eason_lin@trend.com.tw>
2018-09-05 09:58:21 +0800

    add docker file and refine log

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t trend-hearts .
+docker tag trend-hearts ai.registry.trendmicro.com/168/trend-hearts:practice
+docker push ai.registry.trendmicro.com/168/trend-hearts:practice
---
+++ upload.sh
@@ -0,0 +1,2 @@
+docker tag trend-hearts ai.registry.trendmicro.com/168/trend-hearts:rank
+docker push ai.registry.trendmicro.com/168/trend-hearts:rank
